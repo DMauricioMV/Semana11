7943a70d9866c54bc7c604fccb1da005479325dd
1107513806 <1107513806@SC309C20.academico.icesi.edu.co>
2016-04-07 09:49:57 -0500

    filtro uno y dos

---
+++ Ejercicio_S11A/src/FiltroUno.java
@@ -0,0 +1,15 @@
+
+public class FiltroUno {
+	int x;
+	int y;
+	int ancho;
+	int alto;
+public FiltroUno(int x, int y, int alto, int ancho){
+	
+	this.x= x;
+	this.y=y;
+	this.ancho=ancho;
+	this.alto= alto;
+	
+}
+}
